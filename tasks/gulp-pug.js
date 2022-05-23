691e588c1063d49dbfc7b830938ed6f595a8c65a
Andrew Chernigovsky <chernigovsky108@gmail.com>
2022-05-24 00:41:24 +0300

    1

---
+++ tasks/gulp-pug.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const gulp = require('gulp');
+const pug = require('gulp-pug');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+
+module.exports = function (options) {
+
+  return () => {
+    return gulp.src(`./${options.templates}/**/*.pug`)
+    .pipe(
+      pug({
+      })
+    )
+    .pipe(plumber({ errorHandler: notify.onError("<%= error.message %>") }))
+    .pipe(gulp.dest(options.dest));
+}};
